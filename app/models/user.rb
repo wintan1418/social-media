b2f41de4ee602c8024c28cbf2168394cb3c47ac9
oluwadare <wintan1418@gmail.com>
2022-04-26 15:52:44 +0100

    run migrations into schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum:5, maximum: 20 }
+  validates :username, presence: true, uniqueness: true length: { minimum:5, maximum: 20 }
   has_secure_password
 end
