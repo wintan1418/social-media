8545e064a55a520d5705823d69b009cebde812fd
oluwadare <wintan1418@gmail.com>
2022-04-26 11:56:42 +0100

    Add functionalities for navigation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import 'semantic/dist/semantic.js'
 import "channels"
 
 Rails.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'chatrooms#index'
+  get 'login', to: 'sessions#new'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
