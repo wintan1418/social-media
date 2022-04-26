ba4d89fb508be4d1d22570be790d65a94ca6befd
oluwadare <wintan1418@gmail.com>
2022-04-26 17:09:25 +0100

    Implement hirb gem for better backend view of user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "wintanmi", password: "password")
+User.create(username: "wintan", password: "padddword")
+User.create(username: "wint", password: "pasord")
+User.create(username: "winmi", password: "pasddord")
+User.create(username: "wintanmi", password: "password")
