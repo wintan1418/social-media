e1dcbfb7139cc9f0c41dc07b7405ea399778c9c3
oluwadare <wintan1418@gmail.com>
2022-04-26 17:27:19 +0100

    Create resources for messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { minimum:5, maximum: 20 }
+  validates :username, presence: true, uniqueness: true, length: { minimum:5, maximum: 20 },
+            uniqueness: {case_sensitive: false}
+  
+  has_many :messages
   has_secure_password
 end
