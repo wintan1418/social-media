d0c1005b1422dff7401ae4fd5eb78c570cac13e1
oluwadare <wintan1418@gmail.com>
2022-04-26 12:15:36 +0100

    fix dropdown menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import 'semantic/dist/semantic.js'
 import "channels"
-
+import 'semantic-ui-sass'
+import 'jquery'
+ 
+$(document).on('turbolinks:load', function() {
+$('.ui.dropdown').dropdown();
+})
+ 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
