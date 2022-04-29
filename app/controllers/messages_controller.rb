dc67c6d694933e5ef01faaa99ccadc6ac459936b
oluwadare <wintan1418@gmail.com>
2022-04-29 11:43:50 +0100

    Improve display of messages on ui

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,14 +4,18 @@ before_action :require_user
 def create
  message = current_user.messages.build(message_params)
   if message.save
-    redirect_to root_path
-  end
+    ActionCable.server.broadcast "chatroom_channel",
+    mod_message: message_render(message)
+end
 end
 
 private
 
 def message_params
   params.require(:message).permit(:body)
+end
+def message_render(message)
+  render(partial: 'message', locals: {message: message})
 end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,40 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'semantic-ui-sass'
-import 'jquery'
- 
-$(document).on('turbolinks:load', function() {
-$('.ui.dropdown').dropdown();
-$('.message .close').on('click', function() {
-    $(this).closest('.message').transition('fade')
-    ;})
-;
-})
- 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//= require jquery
+// Loads all Semantic javascripts
+//= require semantic-ui
+
+const scroll_bottom = () => {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
+
+const submit_message = () => {
+    $('#message_body').on('keydown', function(e) {
+        if (e.keyCode == 13) {
+            $('button').click();
+            e.target.value = "";
+        }
+    })
+}
+
+require("semantic-ui-sass")
+$(document).on("turbolinks:load", () => {
+    $(".ui.dropdown").dropdown();
+    
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });
+    submit_message();
+    scroll_bottom();
+});
+
+export { scroll_bottom }
